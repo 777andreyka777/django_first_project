2a0da83ce19d5be3c6fed3311b8db73ad244680d
777andreyka777 <ivanov.andrey.vhs777@yandex.ru>
2023-02-28 22:46:48 +0300

    update

--- mysite/views.py
+++ mysite/views.py
@@ -7,3 +7,9 @@ def about(request):
 
 def home(request):
     return render(request, 'home.html')
+
+def reverse(request):
+    text_input = request.GET['input']
+    len_count = len(text_input.split())
+    reverved_text = text_input[::-1]
+    return render(request,'reverse.html',{'input': text_input, 'reversed_text': reverved_text, 'len_count': len_count})
